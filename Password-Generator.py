c7e48e1331816e48f38bdddddfc178fa33e6ef9e
Kieran <kieranjust@gmail.com>
2021-03-03 14:32:43 +0800

    adding a change from the feature branch (pw length)

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,6 +1,23 @@
 import random
 import string
 
-password_characters = random.sample(string.ascii_letters, k=10)
-password = ''.join(password_characters)
-print(password)
+
+def password_generator():
+    while True:
+        try:
+            characters = int(input("How many characters would you like in your password? (Max 50)"))
+
+        except ValueError:
+            print("Please use numeric keys to enter a number between 1-50.")
+            continue
+
+        while characters < 0 or characters > 50:
+            characters = int(input("Please enter a number between 1 and 50."))
+
+        password_characters = random.sample(string.ascii_letters, k=characters)
+        password = ''.join(password_characters)
+        print(password)
+        break
+
+
+password_generator()
